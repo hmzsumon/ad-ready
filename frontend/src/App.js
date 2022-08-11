a2750f421f3055058c6e0bfdeeb8c996d6afed54
hmzsumon <zsumonn@gmail.com>
2022-08-11 16:44:45 +0600

    test13

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,11 +50,11 @@ function App() {
 
   // disable right click
   // window.addEventListener('contextmenu', (e) => e.preventDefault());
-  window.addEventListener('keydown', (e) => {
-    if (e.keyCode == 123) e.preventDefault();
-    if (e.ctrlKey && e.shiftKey && e.keyCode === 73) e.preventDefault();
-    if (e.ctrlKey && e.shiftKey && e.keyCode === 74) e.preventDefault();
-  });
+  // window.addEventListener('keydown', (e) => {
+  //   if (e.keyCode === 123) e.preventDefault();
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode === 73) e.preventDefault();
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode === 74) e.preventDefault();
+  // });
   return (
     <>
       <Header />
