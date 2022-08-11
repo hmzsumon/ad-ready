42cb4d322365f0f6b607011946ed952aee91e7ef
hmzsumon <zsumonn@gmail.com>
2022-08-11 16:21:23 +0600

    test9

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,17 @@
-import { SnackbarProvider } from 'notistack';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import store from './store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <SnackbarProvider
-        maxSnack={2}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-      >
-        <Router>
-          <App />
-        </Router>
-      </SnackbarProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ frontend2/src/index.js
@@ -0,0 +1,27 @@
+import { SnackbarProvider } from 'notistack';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import store from './store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <SnackbarProvider
+        maxSnack={2}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+      >
+        <Router>
+          <App />
+        </Router>
+      </SnackbarProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
